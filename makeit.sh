39c11cc39326c9b52348f30dce87542184fce04f
Jack Wasey <jack@jackwasey.com>
2015-09-21 09:05:42 -0400

    .

--- makeit.sh
+++ makeit.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # g++ -std=c++11 parallel_stdlib.cpp
-g++ -D_GLIBCXX_PARALLEL -fopenmp -std=c++11 parallel_stdlib.cpp
+g++ -D_GLIBCXX_PARALLEL -fopenmp -std=c++11 parallel_stdlib_test.cpp
