9c85e6c719a8b63c23d3d59c4fec43ac24a74782
Jack Wasey <jack@jackwasey.com>
2015-09-21 09:56:07 -0400

    working, shows all threads in action, and much faster this way

--- parallel_stdlib_test.cpp
+++ parallel_stdlib_test.cpp
@@ -2,20 +2,19 @@
 #include <chrono>
 #include <vector>
 #include <random>
+//#include <algorithm>
 #include <parallel/algorithm>
 #include <functional>
-//#include <algorithm>
+#include <boost/progress.hpp>
 
-
-bool myfunction (int i,int j) { return (i<j); }
-
-struct myclass {
-  bool operator() (int i,int j) { return (i<j);}
-} myobject;
+void sp(std::vector<int>& v); 
+void ss(std::vector<int>& v2); 
 
 int main() {
 
-int n = 10;
+boost::progress_timer t_main;
+
+int n = 50000000;
 std::vector<int> v(n);
 //v.reserve(n);
 printf("filling with random\n");
@@ -25,9 +24,6 @@ std::uniform_int_distribution<int> dis(0, n*10);
 auto myrnd = std::bind(dis, gen);
 
 std::generate(v.begin(), v.end(), myrnd);
-//for(int i = 0; i != n; ++i){
-//  v.push_back(myrnd());
-//}
 std::cout << "before sorting, first five of v contains:";
 for (std::vector<int>::iterator it=v.begin(); it!=v.begin()+5; ++it)
   std::cout << ' ' << *it;
@@ -35,11 +31,14 @@ std::cout << '\n';
 printf("sorting\n");
 printf("v length = %i\n", (int)v.size());
 
+std::vector<int> v2(v);
+
 // do the sort
 //std::sort(v.begin(), v.end());
 //std::sort(v.begin(), v.end(), std::greater<int>());
-//force parallel with the last argument:
-std::sort(v.begin(), v.end(), __gnu_parallel::sequential_tag());
+
+sp(v);
+ss(v2);
 
 std::cout << "first five of v after sorting contains:";
 for (std::vector<int>::iterator it=v.begin(); it!=v.begin()+5; ++it)
@@ -49,5 +48,16 @@ std::cout << '\n';
 printf("first = %u\n", v[0]);
 printf("last  = %u\n", v[n-1]);
 return 0;
+}
+
+void sp(std::vector<int>& v) {
+boost::progress_timer t;
+std::sort(v.begin(), v.end());
+printf("timing is wrong! it sums time for each thread, but this interestingly gives estimate of the threading overhead");
+}
+
+void ss(std::vector<int>& v2) {
+boost::progress_timer t;
+std::sort(v2.begin(), v2.end(), __gnu_parallel::sequential_tag());
 }
 
